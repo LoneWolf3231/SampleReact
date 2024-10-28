c2e9128495f9c773806e1be7e4ba4854dc4b6352
Sumukha Raghavan M <112052089+LoneWolf3231@users.noreply.github.com>
2024-10-28 23:07:49 +0530

    Update and rename App.js to App.jsx

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TableComponent from './Components/TableComponent';
+import DownloadButton from './Components/DownloadButton';
+
+const App = () => {
+  return (
+    <div>
+      <DownloadButton>
+        <TableComponent />
+      </DownloadButton>
+    </div>
+  );
+};
+
+export default App;
+
+
+####Original Solution####
+
